33aeb2c1831d538a77a875d53ba8376a646670f2
Teo Lee <ttslee@users.noreply.github.com>
2021-01-29 20:29:25 -0800

    scripts/folders added

---
+++ GameJam2021/Assets/_Scripts/Utilities/Enums.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+    public enum InteractableType
+    {
+        KEY,
+    }
+}
