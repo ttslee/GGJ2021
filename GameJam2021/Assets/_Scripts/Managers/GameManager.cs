d896106479f0818c3393b77b43083e5f890cbab0
Teo Lee <ttslee@users.noreply.github.com>
2021-01-29 20:55:45 -0800

    interactable and gamemanager Dictionary

--- GameJam2021/Assets/_Scripts/Managers/GameManager.cs
+++ GameJam2021/Assets/_Scripts/Managers/GameManager.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Dictionary<InteractableType,bool> checkpoints;
+   
+    
 }
