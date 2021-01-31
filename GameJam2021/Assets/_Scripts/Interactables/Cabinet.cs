fcb3c50090e6587acc6de64a267cad452d77d179
Teo Lee <ttslee@users.noreply.github.com>
2021-01-31 09:30:16 -0800

    cabinet

--- GameJam2021/Assets/_Scripts/Interactables/Cabinet.cs
+++ GameJam2021/Assets/_Scripts/Interactables/Cabinet.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Cabinet : Interactable
 {
+    [SerializeField]private GameObject cabinetRef;
+    [SerializeField]private AudioClip lockedSound;
     public override void Interact()
     {
         base.Interact();
-        
+        previousCheckpoint = GameManager.Instance.checkpoints[InteractableType.KEY];
+        if(!previousCheckpoint)
+        {
+            AudioManager.Instance.PlayEffect(lockedSound);
+            DialogueManager.Instance.EnableDialogue(this, incompleteTextDialogue);
+        }
+        else
+        {
+            AudioManager.Instance.PlayEffect(selectionSound);
+            DialogueManager.Instance.EnableDialogue(this, readyTextDialogue);
+        }
     }
 
     public override void Finished()
     {
-        base.Finished();
+        if(previousCheckpoint)
+            OpenCabinet();
+        else
+            base.Finished();
+    }
+
+    private void OpenCabinet()
+    {
+        cabinetRef.SetActive(true);
     }
 }
